0d556fd71bde30f8dcd8a82c04a15114c39c61c5
sqmasep <leblond.romain@orange.fr>
2023-02-08 10:44:39 +0100

    fixed wrong react query version

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,10 @@ import { useState } from "react";
 import { useLocation, useRoutes } from "react-router-dom";
 import theme from "./lib/mui/theme";
 import Home from "./pages/Home";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "react-query-devtools";
+
+const queryClient = new QueryClient();
 
 const App: React.FC = () => {
   const element = useRoutes([{ path: "/", element: <Home /> }]);
@@ -12,10 +16,13 @@ const App: React.FC = () => {
   if (!element) return null;
 
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      {React.cloneElement(element, { key: location.pathname })}
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {React.cloneElement(element, { key: location.pathname })}
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
 
--- client/src/lib/mui/theme.ts
+++ client/src/lib/mui/theme.ts
@@ -1,12 +1,19 @@
 import { createTheme } from "@mui/material";
+import { deepPurple, indigo } from "@mui/material/colors";
 
 const theme = createTheme({
   palette: {
     background: {
-      default: "#222",
+      default: "#070933",
     },
     primary: {
-      main: "#f00",
+      main: deepPurple[300],
+    },
+  },
+  typography: {
+    allVariants: {
+      fontFamily: "Neue Plak",
+      fontSize: "1.125rem",
     },
   },
 });
